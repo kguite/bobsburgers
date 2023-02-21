7a2f516af34aa9018eb71e395c9a4f5996c6371f
katie-guite <kguite@gmail.com>
2023-02-21 12:45:23 -0800

    map through characters

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,26 +1,12 @@
 import React from "react";
 
-
-
 function CharacterCard(character) {
-
-   
 
     return (
         <div className="character-card">
-            
-            <div className="character--image">
-                <img src={character.image} alt="character" />
-            </div>
-
-            <div className="character--name">
-                <h3>{character.name}</h3>
-            </div>
-
-            <div className="character--desc">
-                <p>{character.desc}</p>
-            </div>
-
+            <img className="character--image" src={character.image} alt="character" />
+            <h3 lassName="character--name">{character.name}</h3>
+            <p className="character--desc">{character.desc}</p>
         </div>
     )
 }
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -7,40 +7,52 @@ import Gene from "../images/Gene.png";
 import Louise from "../images/Louise.png";
 
 
-
-
 function Characters(){
+
+    const characters = [
+        {
+            image:Bob,
+            name:"Bob Belcher",
+            desc:"Would kiss Thanksgiving."
+        },
+        {
+            image:Linda,
+            name:"Linda Belcher",
+            desc:"Linda doesn't get drunk, she just has fun."
+        },
+        {
+            image:Tina,
+            name:"Tina Belcher",
+            desc:"A smart, strong, sensual woman."
+        },
+        {
+            image:Gene,
+            name:"Gene Belcher",
+            desc:"Leader of the Itty Bitty Ditty Committee."
+        },
+        {
+            image:Louise,
+            name:"Gene Belcher",
+            desc:"Totally not into Boyz 4 Now."
+        },
+    ]
 
 
     return (
+        
         <div className="characters" id="characters">
             <h2 className="characters--title">The Belcher Family</h2>
-            <div className="character-cards">
-            <CharacterCard 
-                image={Bob}
-                name="Bob Belcher"
-                desc="Would kiss Thanksgiving."
-                />
-            <CharacterCard 
-                image={Linda}
-                name="Linda Belcher"
-                desc="Linda doesn't get drunk, she just has fun."
-                />
-            <CharacterCard 
-                image={Tina}
-                name="Tina Belcher"
-                desc="A smart, strong, sensual woman."/>
-            <CharacterCard 
-                image={Gene}
-                name="Gene Belcher"
-                desc="Leader of the Itty Bitty Ditty Committee."/>
-            <CharacterCard 
-                image={Louise}
-                name="Louise Belcher"
-                desc="Not into Boyz 4 Now."/>
-            </div>
+            
+            {characters.map((character) =>{
+                return (
+                    <CharacterCard 
+                        image={character.image} 
+                        name={character.name} 
+                        desc={character.desc}
+                    />
+                );})}
         </div>
-    )
+    );
 }
 
 export default Characters
