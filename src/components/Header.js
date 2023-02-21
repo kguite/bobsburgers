ea768e8361acd803e52c483ba8048d2dbc6d0aa6
katie-guite <kguite@gmail.com>
2023-02-20 23:22:07 -0800

    list styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,14 @@ function Header() {
             <img src={bobsBurgersName} alt="Bob's Burgers" className="header--img" />
             <div className="header--text">
                 <p>This Bob's Burgers site was built from scratch using React.</p>
-                    <ul>Future additions will include:
-                        <li>Burger of the Day info pulled from the API (for now, it's in a file called burgerData.js)</li>
+                    <ul><strong>Future additions will include:</strong>
+                        <li>Burger of the Day info pulled from the API <br />
+                        (for now, it's in a file called burgerData.js)</li>
                         <li>A form where you can create your own Burger of the Day</li>
+                        <li>A proper footer</li>
+                        <li>Lots more REACT practice and features</li>
                     </ul>
+                    <p>Thanks for visiting my practice project!</p>
             </div>
         </header>
 
