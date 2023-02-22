5df3a4318b842be6ffe59b7285030ff6f1f70f30
katie-guite <kguite@gmail.com>
2023-02-21 20:58:10 -0800

    fix typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,13 +13,13 @@ function Header() {
                     <ul className="header--ul"><strong>Current Features:</strong>
                         <li><strong>- Characters</strong> are stored in an array, and the map function is used to iterate through them and display them using a CharacterCard component. </li>
                         <li><strong>- Burger of the Day</strong> info is pulled from the <a href="https://www.bobsburgersapi.com">Bob's Burgers API</a> with a Math.Random() function used to randomize the result.</li>
-                        <li><strong>- Quiz</strong> utilizes useState and useEffect</li>
+                        <li><strong>- Quiz</strong> utilizes useState and useEffect.</li>
                     </ul>
 
                     <ul className="header--ul"><strong>Future additions:</strong>
-                        <li>- A form to create your own Burger of the Day</li>
-                        <li>- Refactoring the CSS into SASS</li>
-                        <li>- Prettier styling</li>
+                        <li>- A form to create your own Burger of the Day.</li>
+                        <li>- Refactoring the CSS into SASS.</li>
+                        <li>- Prettier styling.</li>
                     </ul>
                     <p>Thanks for visiting my practice project!</p>
                     <p><a href="https://github.com/kguite/bobsburgers">View the GitHub</a></p>
